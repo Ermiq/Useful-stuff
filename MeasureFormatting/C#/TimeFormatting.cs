55a55a1ddc0d227c93bb3c18e2af7535c7826413
Ermiq <58442318+Ermiq@users.noreply.github.com>
2021-03-29 10:33:15 +0300

    Update TimeFormatting.cs

--- MeasureFormatting/C#/TimeFormatting.cs
+++ MeasureFormatting/C#/TimeFormatting.cs
@@ -1,23 +1,26 @@
 public static void GetTimeFromSeconds(float secondsTotal, out int s, out int m, out int h, out int d, out int y)
 {
 	s = m = h = d = y = 0;
-	float R = secondsTotal;
 
 	s = (int)(secondsTotal % 60);
-	R -= s;
-	if (R < 60)
+	secondsTotal -= s;
+	if (secondsTotal < 60)
 		return;
-	m = (int)((secondsTotal - s) / 60 % 60);
-	R -= m * 60;
-	if (R < 3600)
+
+	m = (int)(secondsTotal / 60 % 60);
+	secondsTotal -= m * 60;
+	if (secondsTotal < 3600)
 		return;
-	h = (int)(R / 3600 % 24);
-	R -= h * 3600;
-	if (R < 86400)
+
+	h = (int)(secondsTotal / 3600 % 24);
+	secondsTotal -= h * 3600;
+	if (secondsTotal < 86400)
 		return;
-	d = (int)(R / 86400 % 365);
-	R -= d * 86400;
-	if (R < 31536000)
+
+	d = (int)(secondsTotal / 86400 % 365);
+	secondsTotal -= d * 86400;
+	if (secondsTotal < 31536000)
 		return;
-	y = (int)(R / 31536000);
+
+	y = (int)(secondsTotal / 31536000);
 }
