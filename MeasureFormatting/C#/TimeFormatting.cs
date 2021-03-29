aef68890d4713e126ff5fd20a79f0aee00e05186
Ermiq <58442318+Ermiq@users.noreply.github.com>
2021-03-29 10:55:56 +0300

    update TimeFormatting.cs

--- MeasureFormatting/C#/TimeFormatting.cs
+++ MeasureFormatting/C#/TimeFormatting.cs
@@ -3,22 +3,36 @@ public static void GetTimeFromSeconds(float secondsTotal, out int s, out int m, 
 	s = m = h = d = y = 0;
 
 	s = (int)(secondsTotal % 60);
+	// substruct the seconds remainder from the total amount (75 - 15 = 60, 125 - 5 = 120).
 	secondsTotal -= s;
+	// of nothing left then it was less than 1 minute (45 - 0 = 45).
 	if (secondsTotal < 60)
 		return;
-
+	
+	// secondsTotal / 60 => how many minutes total
+	// % 60 => how many minutes remain after splitting to whole hours
 	m = (int)(secondsTotal / 60 % 60);
+	// substruct the minutes remainder from the total amount (every minute takes 60 secs from the total)
 	secondsTotal -= m * 60;
+	// if there's not enough seconds remain in the total to get at least 1 whole hour (3600 secs)
+	// then it means there was  less than 1 hour.
 	if (secondsTotal < 3600)
 		return;
 
+	// divide by 3600 => how many hours remain
+	// % 24 => what will remain after splitting to days (24 hours)
 	h = (int)(secondsTotal / 3600 % 24);
+	// every while hour takes 3600 secs from the total
 	secondsTotal -= h * 3600;
+	// 24 hours = 86400 seconds.
+	// If there's less remaining than it was less than 24 hours.
 	if (secondsTotal < 86400)
 		return;
 
 	d = (int)(secondsTotal / 86400 % 365);
 	secondsTotal -= d * 86400;
+	// 1 year = 31536000 secs.
+	// is there enough secs remaining to get a whole year?
 	if (secondsTotal < 31536000)
 		return;
 
